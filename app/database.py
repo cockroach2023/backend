f00ef6ae28de94cef007fc8112294cf242002f4e
bong-u <bongudev@gmail.com>
2023-11-16 18:37:14 +0900

    Fix: postgres db 사용하도록 수정

--- app/database.py
+++ app/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.utils.settings import Settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-# SQLALCHEMY_DATABASE_URL = Settings().dict()["POSTGRES_URL"]
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = Settings().dict()["POSTGRES_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
